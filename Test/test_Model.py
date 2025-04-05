49e224f72c3d7b89eeea645f1bb7ed1bb2f473e9
Phạm Phú Hòa <phamhoa23us@gmail.com>
2025-04-05 18:56:55 +0700

    add: test_Model final

--- Test/test_Model.py
+++ Test/test_Model.py
@@ -1,11 +1,17 @@
+from tensorflow.python.keras.saving.saved_model.serialized_attributes import metrics
+
 from PreProcessing.VehicleDataPreprocessor import VehicleDataPreprocessor
 from PreProcessing.FeatureSelector import FeatureSelector
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from LinearModel.LinearModel import StandardScaler
-from models.model import LinearRegression
+from LinearModel.StandardScaler import StandardScaler
+# from models.model import LinearRegression
+from LinearModel.LinearModel import LinearRegression
 from utils.data_preprocessing import train_test_split
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def visualize_results(X_test, y_test, y_pred, title="Predictions vs. Actual Values"):
@@ -43,31 +49,106 @@ if __name__ == "__main__":
     y = df["Log_Price"]
     X = df.drop("Log_Price", axis=1)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.1, random_state=43)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
+                                                        random_state=42)
+    y_train, y_test = y_train.to_numpy(), y_test.to_numpy()
 
-    X_train = FeatureSelector._non_linearize_features(X_train)
-    X_test = FeatureSelector._non_linearize_features(X_test)
+    X_train_o = FeatureSelector._non_linearize_features(X_train)
+    X_test_o = FeatureSelector._non_linearize_features(X_test)
 
-    _, X_train = FeatureSelector.get_df(
-        X_train, model_id=1, get_Log_Price=False)
-    _, X_test = FeatureSelector.get_df(X_test, model_id=1, get_Log_Price=False)
+    # print(X_train.columns)
 
-    X_train, X_test, y_train, y_test = X_train.to_numpy(), X_test.to_numpy(), y_train.to_numpy().flatten(), y_test.to_numpy().flatten()
+    # print(X_train.shape)
 
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    feats, X_train = FeatureSelector.get_features(X_train)
+    _, X_test = FeatureSelector.get_features(X_test)
+    corr_matrix = X_train_o.corr()
+    plt.figure(figsize=(10, 8))
 
-    # print(X_test)
+    # Tạo heatmap
+    mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
+    sns.heatmap(corr_matrix,
+                annot=True,  # Hiển thị giá trị tương quan
+                mask=mask,  # Chỉ hiển thị nửa dưới của ma trận
+                cmap='coolwarm',  # Bảng màu
+                vmin=-1, vmax=1,  # Giới hạn giá trị
+                fmt='.2f',  # Format số thập phân
+                square=True,  # Ô vuông
+                linewidths=.5)  # Độ rộng đường viền
+
+    # Thêm tiêu đề
+    plt.title('Ma trận tương quan', fontsize=15)
+
+    # Hiển thị plot
+    plt.tight_layout()
+    plt.show()
+    for i in range(1,5):
+        print(f" === MODEL {i} ===")
+        feats, X_train = FeatureSelector.get_df(X_train_o, model_id=i, get_Log_Price=False)
+        _, X_test = FeatureSelector.get_df(X_test_o, model_id=i, get_Log_Price=False)
+
+        # print(X_train.shape)
+
+        X_train, X_test= X_train.to_numpy(), X_test.to_numpy()
+
+        # print(X_train.shape)
+
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        model = LinearRegression(
+            optimizer='adam',
+            learning_rate=0.01,
+            max_iter=5000,
+            l2_penalty=0.005,
+            l1_penalty=0.005,
+            random_state=42,
+            tol=1e-10,
+        )
+
+        # Huấn luyện mô hình
+        model.fit(X_train, y_train, verbose=False)
+
+        # Dự đoán và đánh giá
+        # y_pred_train = model.predict(X_train)
+        # metrics = model._evaluate(y_train, y_pred_train)
+        # print("Train metrics: ", metrics)
+        # visualize_results(X_train, y_train, y_pred_train)
+        y_pred = model.predict(X_test)
+        metrics = model._evaluate(y_test, y_pred)
+        print("Test metrics:", metrics)
+
+        # Cross-validation
+        cv_results = model.cross_validate(X_train, y_train, n_folds=5, verbose=False)
+        y_pred = model.predict(X_test)
+        metrics = model._evaluate(y_test, y_pred)
+        print("Test metrics:", metrics)
+        feats.insert(0, "Intercept")
+        # for col, weight in zip(feats, model.optimizer.weights):
+        #     if np.abs(weight) >= 0.01:
+        #         print(f"'{col}', {weight}")
+
+
+        visualize_results(X_test, y_test, y_pred)
+        visualize_results(X_test, np.exp(y_test), np.exp(y_pred))
+
+    # print(X_train[:,1].max())
+
+
+    # print(X_train.shape)
+
+    # print(X_test.mean())
     # print(y_test)
 
     # Huấn luyện với normal equation
-    print("\n=== Huấn luyện bằng Normal Equation ===")
-    model1 = LinearRegression(optimizer='normal_equation')
-    model1.fit(X_train, y_train, verbose=True)
-    model1.evaluate(X_test, y_test)
-    mean_score, _ = model1.cross_validate(X_train, y_train)
-    print("Mean score of cross validation:", mean_score)
+    # print("\n=== Huấn luyện bằng Normal Equation ===")
+    # model1 = LinearRegression(optimizer='normal')
+    # # print(X_train.shape)
+    # model1.fit(X_train, y_train, verbose=True)
+    # y_pred_test = model1.predict(X_test)
+    # model1._evaluate(y_test, y_pred_test)
+    # mean_score, _ = model1.cross_validate(X_train, y_train)
+    # print("Mean score of cross validation:", mean_score)
 
     # # Huấn luyện với gradient descent
     # print("\n=== Huấn luyện bằng Gradient Descent ===")
