1eb2b00ea77ad00783b179e6ae2cd59250d78f19
Phạm Phú Hòa <phamhoa23us@gmail.com>
2025-04-05 18:55:22 +0700

    update: Feature Selection (Final one)

--- PreProcessing/FeatureSelector.py
+++ PreProcessing/FeatureSelector.py
@@ -3,188 +3,124 @@ import pandas as pd
 
 class FeatureSelector(object):
     features = {
-        0: [
+        1: [
+            'Length',
+            'Fuel Tank Capacity',
+            'Max_Power_Value',
+            'Max_Torque_Value',
+            'Make_encoded',
+            'Fuel_Petrol',
+            'Transmission_is_Automatic',
+            'Owner_Third_Owner',
+            'Seating_2',
+            'Volume',
+            'Log_Max_Power_Value',
+            'Power_Density',
+            'Length_Width_interaction',
+
+            'Age',
+            'Age_Length',
+            'Age_Volume',
+            'Age_Log_Kilometer',
+            'Make_encoded_log',
+        ],
+        2: [
+            'Kilometer',
+
+            'Length',
+            'Width',
+            'Height',
+
+            'Fuel Tank Capacity',
+            'Engine_Value',
+            'Max_Power_Value',
+            'Max_Power_RPM',
+            'Max_Torque_Value',
+            'Max_Torque_RPM',
+
+            'Make_encoded',
+
+            'Fuel_Diesel',
+            'Fuel_Petrol',
+
+            'Transmission_is_Automatic',
+
+            'Owner_Fourth_Plus_Owner',
+            'Owner_New',
+
+            'Seating_2',
+            'Seating_4',
+
+            'Volume',
+
+            'Power_to_Weight',
+
+            'Log_Kilometer',
+            'Log_Max_Power_Value',
+            'Log_Max_Torque_Value',
+
+            'Power_Density',
+            'Torque_Density',
+
+            'Power_Torque_Ratio',
+
+            'Length_Width_interaction',
+
+            'Year_Inverse',
+            'Age',
+            'Age_squared',
+            'Log_Age',
+            'Age_Length',
+            'Age_Height',
+            'Age_Volume',
+            'Age_Log_Kilometer',
+
+            'Kilometer_per_Year',
+            'Log_Kilometer_Length',
+            'Log_Kilometer_Power',
+            'Log_Kilometer_Torque',
+
+            'Make_encoded_sqrt',
+            'Make_encoded_log',
+            'Make_encoded_squared',
+
+            'Power_to_CC',
+            'Torque_to_CC',
+            'Power_to_Torque',
+            'RPM_Ratio',
+        ],
+        3: [
+            'Age',
             'Log_Max_Power_Value',
             'Age_Log_Kilometer',
             'Make_encoded',
             'Fuel Tank Capacity',
-            'Length_Width_interaction',
-            'Max_Power_Value',
-            'Age_Volume',
-            'Age_Height',
-            'Engine_Value',
-            'Age_Kilometer',
-            'Power_to_CC',
-            'Age',
-            "Log_Kilometer",
-            'Log_Kilometer_Length',
-            'Log_Kilometer_Power',
-
-            'Fuel_Others',
             'Fuel_Petrol',
-
-            'Owner_Fourth_Plus_Owner',
-
-            'Seating_2',
-            'Seating_4',
-        ],
-        1: [
-            # Numerical
-            # # Features cơ bản
-            # "Year",
-            # 'Sin_Year',
-            # 'Year_Inverse',
-            # "Age",
-            "Log_Age",
-            # 'Sin_Age',
-            "Age_squared",
-            "Age_Log_Kilometer",
-            'Age_Length',
-            # 'Age_Height',
-            # "Kilometer",
-            "Log_Kilometer",
-            'Log_Kilometer_Length',
-            'Log_Kilometer_Power',
-            # 'Log_Kilometer_Torque',
-
-            "Make_encoded",
-            # 'Drivetrain_encoded',
-
-            # # Features quan trọng về thông số kỹ thuật
-            'Log_Max_Power_Value',
-            # 'Max_Power_Value',
-            # 'Max_Power_RPM',
-            'Log_Max_Torque_Value',
-            # 'Max_Torque_Value',
-            "Fuel Tank Capacity",
-
-            # # Features tính toán
-            # 'Efficiency',
-            # 'Power_to_CC',
-
-            # # Features kích thước
-            "Length",
-            'Volume',
-            # 'Length_Width_interaction',
-
-            # # Các features kết hợp
-            'Age_Volume',
-            # 'Age_Kilometer',
-            'Kilometer_per_Year',
-            # 'Power_to_Torque',
-            # 'RPM_Ratio',
-
-            # Categorical
-            'Fuel_CNG',
-            'Fuel_Others',
-            'Fuel_Petrol',
-
             'Transmission_is_Automatic',
-
-            # 'Owner_First_Owner',
-            # 'Owner_Fourth_Plus_Owner',
-            # 'Owner_New',
-            # 'Owner_Second_Owner',
-            # 'Owner_Third_Owner',
-
+            'Owner_Fourth_Plus_Owner',
             'Seating_2',
             'Seating_4',
-            # 'Seating_6',
-            # 'Seating_7',
-            # 'Seating_8'
-        ],
-        2: [
-            # Numerical
-            "Year",
-            "Kilometer",
-            "Width",
-            "Length",
-            "Fuel Tank Capacity",
-            "Engine_Value",
-            'Max_Power_Value',
-            "Max_Power_RPM",
-            "Max_Torque_Value",
-            "Max_Torque_RPM",
-            "Make_encoded",
-            "Age_Kilometer",
-            "Kilometer_per_Year",
-
-            # Categorical
-            "Color_Silver",
-
-            'Seating_2',
-
-            "Transmission_is_Automatic",
-
-            "Seller_Commercial Registration",
-
-            'Fuel_CNG',
-            'Fuel_Others',
-            'Fuel_Petrol',
-
-            'Owner_First_Owner',
-            'Owner_Fourth_Plus_Owner',
-            'Owner_New',
-            'Owner_Second_Owner',
-            'Owner_Third_Owner',
-        ],
-        3: [
-            # Numerical
             'Length',
-            'Year',
-            'Make_encoded',
-            'Max_Power_Value',
-            'Width',
-            'Power_to_CC',
-            'Fuel_Petrol',
-            'Max_Torque_Value',
-            'Power_to_Torque',
-            'Kilometer',
-
-            # Categorical
-            'Transmission_is_Automatic',
-
-            'Fuel_CNG',
-            'Fuel_Others',
-            'Fuel_Petrol',
-
-            'Seating_2',
-            'Seating_4',
-            'Seating_6',
-            'Seating_7',
-            'Seating_8'
         ],
         4: [
-            # Numerical
-            # # Features cơ bản
-            'Year',
-            'Kilometer',
+            'Age',
+            'Age_Length',
+            'Age_Volume',
+            'Age_Log_Kilometer',
             'Make_encoded',
 
-            # # Features quan trọng về thông số kỹ thuật
-            'Max_Power_Value', 'Engine_Value', 'Fuel Tank Capacity',
+            'Fuel Tank Capacity',
 
-            # # Features tính toán
-            'Efficiency', 'Power_to_CC',
+            'Length',
+            'Width',
+            'Length_Width_interaction',
 
-            # Categorical
-            'Fuel_CNG',
-            'Fuel_Others',
-            'Fuel_Petrol',
+            'Power_Density',
 
-            'Owner_First_Owner',
-            'Owner_Fourth_Plus_Owner',
-            'Owner_New',
-            'Owner_Second_Owner',
-            'Owner_Third_Owner',
+            'Color_Silver',
 
             'Seating_2',
             'Seating_4',
-            'Seating_6',
-            'Seating_7',
-            'Seating_8',
-
-            'Transmission_is_Automatic'
         ]
     }
     def __init__(self):
@@ -224,11 +160,16 @@ class FeatureSelector(object):
         df['Log_Kilometer_Power'] = df['Log_Kilometer'] * df['Max_Power_Value']
         df['Log_Kilometer_Torque'] = df['Log_Kilometer'] * df['Max_Torque_Value']
 
+        df['Make_encoded_sqrt'] = np.sqrt(df['Make_encoded'])
+        df['Make_encoded_log'] = np.log(df['Make_encoded'])
+        df['Make_encoded_squared'] = df['Make_encoded'] ** 2
+
         df['Power_to_Weight'] = df['Max_Power_Value'] / df['Volume'].clip(lower=1)
         df['Power_to_CC'] = df['Max_Power_Value'] / df['Engine_Value'].clip(lower=1)
         df['Torque_to_CC'] = df['Max_Torque_Value'] / df['Engine_Value'].clip(lower=1)
         df['Power_to_Torque'] = df['Max_Power_Value'] / df['Max_Torque_Value'].clip(lower=1)
         df['RPM_Ratio'] = df['Max_Power_RPM'] / df['Max_Torque_RPM'].clip(lower=1)
+        df.drop(['Model', 'Location'], axis=1, inplace=True)
 
         return df
 
@@ -237,3 +178,9 @@ class FeatureSelector(object):
         """Trả về list chứa các tên columns và df"""
         if get_Log_Price: return cls.features[model_id] + ['Log_Price'], df[cls.features[model_id] + ['Log_Price']]
         else: return cls.features[model_id], df[cls.features[model_id]]
+
+    @classmethod
+    def get_features(cls, df):
+        df = df.select_dtypes(include=[np.number])
+        cols = df.columns
+        return list(cols), df
